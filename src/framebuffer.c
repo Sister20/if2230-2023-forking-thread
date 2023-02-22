23b51d5b6eb7a81a7ede8f5db1c4779f19ac40f9
Fatih20 <fnri39@protonmail.com>
2023-02-22 21:51:08 +0700

    Show our name in the OS's home.

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -3,28 +3,46 @@
 #include "lib-header/stdmem.h"
 #include "lib-header/portio.h"
 
-void framebuffer_set_cursor(uint8_t r, uint8_t c) {
+void framebuffer_set_cursor(uint8_t r, uint8_t c)
+{
     // Set cursor to specified location. Row and column starts from 0
     uint16_t pos = r * 80 + c;
-	out(0x3D4, 0x0F);
-	out(0x3D5, (uint8_t) (pos & 0xFF));
-	out(0x3D4, 0x0E);
-	out(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+    out(0x3D4, 0x0F);
+    out(0x3D5, (uint8_t)(pos & 0xFF));
+    out(0x3D4, 0x0E);
+    out(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
 }
 
-void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
+void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
+{
     // Set framebuffer character and color with corresponding parameter values.
     uint16_t attrib = (bg << 4) | (fg & 0x0F);
-    volatile uint16_t * location;
+    volatile uint16_t *location;
     location = (volatile uint16_t *)MEMORY_FRAMEBUFFER + (row * 80 + col);
     *location = c | (attrib << 8);
 }
 
-void framebuffer_clear(void) {
+void framebuffer_write_row(uint8_t row, uint8_t startCol, char c[], uint8_t fg, uint8_t bg)
+{
+    int nthChar = 0;
+    int i = startCol - 1;
+
+    while (i < 80 && c[nthChar] != '\0')
+    {
+        framebuffer_write(row, i, c[nthChar], fg, bg);
+        i++;
+        nthChar++;
+    }
+}
+
+void framebuffer_clear(void)
+{
     // Set all cell in framebuffer character to 0x00 (empty character)
     // and color to 0x07 (gray character & black background)
-    for(int i = 0; i < 25; i++){
-        for(int j = 0; j < 80; j++){
+    for (int i = 0; i < 25; i++)
+    {
+        for (int j = 0; j < 80; j++)
+        {
             framebuffer_write(i, j, 0x00, 0x7, 0x0);
         }
     }
--- src/kernel.c
+++ src/kernel.c
@@ -4,13 +4,19 @@
 #include "lib-header/framebuffer.h"
 #include "lib-header/kernel_loader.h"
 
-void kernel_setup(void) {
+void kernel_setup(void)
+{
     enter_protected_mode(&_gdt_gdtr);
     framebuffer_clear();
-    framebuffer_write(3, 8,  'H', 0, 0xF);
-    framebuffer_write(3, 9,  'a', 0, 0xF);
-    framebuffer_write(3, 10, 'i', 0, 0xF);
-    framebuffer_write(3, 11, '!', 0, 0xF);
-    framebuffer_set_cursor(3, 10);
-    while (TRUE);
+    framebuffer_write_row(3, 8, "Hai!", 0, 0xF);
+    framebuffer_write_row(4, 8, "We are", 0, 0xF);
+    framebuffer_write_row(5, 8, "Michael Jonathan H.", 0xF, 0);
+    framebuffer_write_row(6, 8, "Fatih N.R.I.", 0xF, 0);
+    framebuffer_write_row(7, 8, "Johannes Lee", 0xF, 0);
+    framebuffer_write_row(8, 8, "Hari Sudewa", 0xF, 0);
+    framebuffer_write_row(9, 8, "Johann Christan Kandani", 0xF, 0);
+    framebuffer_write_row(10, 8, "And we'll ready to be forkin' all over the place", 0, 0xF);
+    framebuffer_set_cursor(3, 7);
+    while (TRUE)
+        ;
 }
