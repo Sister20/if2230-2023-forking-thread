5b8d9fdb65a23537a8c2d9c628cf52c61cc75a06
Fatih20 <fnri39@protonmail.com>
2023-03-28 00:17:41 +0700

    Adjust the kernel codes.

--- src/kernel.c
+++ src/kernel.c
@@ -7,41 +7,51 @@
 #include "lib-header/idt.h"
 #include "lib-header/keyboard.h"
 #include "lib-header/fat32.h"
+#include "lib-header/disk.h"
 
 void kernel_setup(void)
 {
     enter_protected_mode(&_gdt_gdtr);
     pic_remap();
     initialize_idt();
+    activate_keyboard_interrupt();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
     initialize_filesystem_fat32();
+    keyboard_state_activate();
+
+    struct ClusterBuffer cbuf[5];
+    for (uint32_t i = 0; i < 5; i++)
+        for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
+            cbuf[i].buf[j] = i + 'a';
+
+    struct FAT32DriverRequest request = {
+        .buf = cbuf,
+        .name = "ikanaide",
+        .ext = "uwu",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size = 0,
+    };
+
+    write(request); // Create folder "ikanaide"
+    memcpy(request.name, "kano1\0\0\0", 8);
+    write(request); // Create folder "kano1"
+    memcpy(request.name, "ikanaide", 8);
+    delete (request); // Delete first folder, thus creating hole in FS
+
+    memcpy(request.name, "daijoubu", 8);
+    request.buffer_size = 5 * CLUSTER_SIZE;
+    write(request); // Create fragmented file "daijoubu"
+
+    struct ClusterBuffer readcbuf;
+    read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER + 1, 1);
+    // If read properly, readcbuf should filled with 'a'
+
+    request.buffer_size = CLUSTER_SIZE;
+    read(request); // Failed read due not enough buffer size
+    request.buffer_size = 5 * CLUSTER_SIZE;
+    read(request); // Success read on file "daijoubu"
+
     while (TRUE)
-    {
-        keyboard_state_activate();
-    }
-
-    /*
-    enter_protected_mode(&_gdt_gdtr);
-    pic_remap();
-    initialize_idt();
-    framebuffer_clear();
-    framebuffer_set_cursor(0, 0);
-    __asm__("int $0x21");
-    while (TRUE);
-    */
-
-    // enter_protected_mode(&_gdt_gdtr);
-    // framebuffer_clear();
-    // framebuffer_write_row(3, 8, "Hai!", 0, 0xF);
-    // framebuffer_write_row(4, 8, "We are", 0, 0xF);
-    // framebuffer_write_row(5, 8, "Michael Jonathan H.", 0xF, 0);
-    // framebuffer_write_row(6, 8, "Fatih N.R.I.", 0xF, 0);
-    // framebuffer_write_row(7, 8, "Johannes Lee", 0xF, 0);
-    // framebuffer_write_row(8, 8, "Hari Sudewa", 0xF, 0);
-    // framebuffer_write_row(9, 8, "Johann Christan Kandani", 0xF, 0);
-    // framebuffer_write_row(10, 8, "And we'll ready to be forkin' all over the place", 0, 0xF);
-    // framebuffer_set_cursor(3, 7);
-    // while (TRUE)
-    //     ;
+        ;
 }
