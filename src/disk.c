6538390cc858dbff1e08121c3872ba55155ba4d1
Fatih20 <fnri39@protonmail.com>
2023-03-27 10:57:33 +0700

    Attempt to debug initialize fat.

--- src/disk.c
+++ src/disk.c
@@ -1,18 +1,22 @@
 #include "lib-header/disk.h"
 #include "lib-header/portio.h"
+#include "lib-header/framebuffer.h"
 
-static void ATA_busy_wait() {
+static void ATA_busy_wait()
+{
   while (in(0x1F7) & ATA_STATUS_BSY)
     ;
 }
 
-static void ATA_DRQ_wait() {
+static void ATA_DRQ_wait()
+{
   while (!(in(0x1F7) & ATA_STATUS_RDY))
     ;
 }
 
 void read_blocks(void *ptr, uint32_t logical_block_address,
-                 uint8_t block_count) {
+                 uint8_t block_count)
+{
   ATA_busy_wait();
   out(0x1F6, 0xE0 | ((logical_block_address >> 24) & 0xF));
   out(0x1F2, block_count);
@@ -23,7 +27,8 @@ void read_blocks(void *ptr, uint32_t logical_block_address,
 
   uint16_t *target = (uint16_t *)ptr;
 
-  for (uint32_t i = 0; i < block_count; i++) {
+  for (uint32_t i = 0; i < block_count; i++)
+  {
     ATA_busy_wait();
     ATA_DRQ_wait();
     for (uint32_t j = 0; j < HALF_BLOCK_SIZE; j++)
@@ -35,8 +40,16 @@ void read_blocks(void *ptr, uint32_t logical_block_address,
 }
 
 void write_blocks(const void *ptr, uint32_t logical_block_address,
-                  uint8_t block_count) {
+                  uint8_t block_count)
+{
+  framebuffer_clear();
+  framebuffer_write_row(0, 1, "Entering write block", 0xF, 0);
+
   ATA_busy_wait();
+
+  framebuffer_clear();
+  framebuffer_write_row(0, 1, "Finished waiting block initial", 0xF, 0);
+
   out(0x1F6, 0xE0 | ((logical_block_address >> 24) & 0xF));
   out(0x1F2, block_count);
   out(0x1F3, (uint8_t)logical_block_address);
@@ -44,13 +57,22 @@ void write_blocks(const void *ptr, uint32_t logical_block_address,
   out(0x1F5, (uint8_t)(logical_block_address >> 16));
   out(0x1F7, 0x30);
 
-  for (uint32_t i = 0; i < block_count; i++) {
+  framebuffer_write_row(0, 1, "Finished value initialization", 0xF, 0);
+
+  for (uint32_t i = 0; i < block_count; i++)
+  {
+
     ATA_busy_wait();
     ATA_DRQ_wait();
+    framebuffer_clear();
+    framebuffer_write_row(0, 1, "Finished waiting block", 0xF, 0);
     /* Note : uint16_t => 2 bytes, i is current block number to write
        HALF_BLOCK_SIZE*i = block_offset with pointer arithmetic
     */
     for (uint32_t j = 0; j < HALF_BLOCK_SIZE; j++)
       out16(0x1F0, ((uint16_t *)ptr)[HALF_BLOCK_SIZE * i + j]);
   }
+
+  framebuffer_clear();
+  framebuffer_write_row(0, 1, "Exiting write blocks", 0xF, 0);
 }
--- src/fat32.c
+++ src/fat32.c
@@ -1,6 +1,7 @@
 #include "lib-header/fat32.h"
 #include "lib-header/stdmem.h"
 #include "lib-header/stdtype.h"
+#include "lib-header/framebuffer.h"
 
 const uint8_t fs_signature[BLOCK_SIZE] = {
     'C',
@@ -89,25 +90,33 @@ const uint8_t fs_signature[BLOCK_SIZE] = {
 
 uint32_t cluster_to_lba(uint32_t cluster) { return cluster * CLUSTER_BLOCK_COUNT; }
 
-void create_fat32(void) {
-  write_blocks(&fs_signature, BOOT_SECTOR, 1);
-
-  if (is_empty_storage()) {
+void create_fat32(void)
+{
+    framebuffer_write_row(0, 1, "Entering create FAT", 0xF, 0);
     write_blocks(&fs_signature, BOOT_SECTOR, 1);
-    return;
-  }
+    framebuffer_write_row(0, 1, "Exiting create FAT", 0xF, 0);
 
-  //
+    //   if (is_empty_storage()) {
+    //     write_blocks(&fs_signature, BOOT_SECTOR, 1);
+    //     return;
+    //   }
+
+    //
 }
 
-void initialize_filesystem_fat32(void) { create_fat32(); }
-
-bool is_empty_storage() {
-  char buf[BLOCK_SIZE];
-  read_blocks(&buf, 0, 1);
-  return !memcmp(&buf, &fs_signature, BLOCK_SIZE) == 0;
+void initialize_filesystem_fat32(void)
+{
+    framebuffer_write_row(0, 1, "Entering initialization", 0xF, 0);
+    create_fat32();
+    framebuffer_write_row(0, 1, "Exiting initialization", 0xF, 0);
 }
 
+bool is_empty_storage()
+{
+    char buf[BLOCK_SIZE];
+    read_blocks(&buf, 0, 1);
+    return !memcmp(&buf, &fs_signature, BLOCK_SIZE) == 0;
+}
 
 void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_count)
 {
@@ -115,7 +124,6 @@ void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_co
     uint8_t block_count = cluster_to_lba(cluster_count);
     write_blocks(ptr, logical_block_address, block_count);
 }
-
 
 void read_clusters(void *ptr, uint32_t cluster_number, uint8_t cluster_count)
 {
