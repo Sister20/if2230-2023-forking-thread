90a0547004553e2a2d6a1bcf213d76411fb2b254
Genvictus <johannstarkmk42@gmail.com>
2023-03-28 12:13:32 +0700

    add: implementation prototype of CMOS RTC reading

---
+++ src/lib-header/cmosrtc.h
@@ -0,0 +1,60 @@
+#ifndef _CMOSRTC_H
+#define _CMOSRTC_H
+
+#include "stdtype.h"
+
+/* Using reference: https://wiki.osdev.org/CMOS 
+    Register  Contents            Range
+    0x00      Seconds             0–59
+    0x02      Minutes             0–59
+    0x04      Hours               0–23 in 24-hour mode, 
+                                  1–12 in 12-hour mode, highest bit set if pm
+    0x06      Weekday             1–7, Sunday = 1
+    0x07      Day of Month        1–31
+    0x08      Month               1–12
+    0x09      Year                0–99
+    0x32      Century (maybe)     19–20?
+    0x0A      Status Register A
+    0x0B      Status Register B
+*/
+
+#define CURRENT_YEAR        2023                            // Change this each year!
+ 
+int century_register = 0x00;                                // Set by ACPI table parsing code if possible
+
+/**
+ * @param second    Range: 0-59
+ * @param minute    Range: 0-59
+ * @param hour      Range: 0-23; uses 24 hour mode
+ *
+ * @param day       Day of month, Range: 1-31
+ * @param month     Range: 1-12
+ * @param year      Range: 0-99
+ */
+struct CMOSTRTC
+{
+    uint8_t second;
+    uint8_t minute;
+    uint8_t hour;
+    uint8_t day;
+    uint8_t month;
+    uint32_t year;
+} __attribute__((packed));
+
+struct CMOSRTC rtc;
+ 
+void out_byte(int port, int value);
+int in_byte(int port);
+ 
+enum {
+      cmos_address = 0x70,
+      cmos_data    = 0x71
+};
+ 
+int get_update_in_progress_flag();
+ 
+uint8_t get_RTC_register(int reg);
+ 
+void read_rtc();
+
+#endif
